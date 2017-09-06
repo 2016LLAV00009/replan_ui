25714cb7db3826291b8084dae20171855127ee68
Sergi Orra <sergi.orra.genero@est.fib.upc.edu>
2017-09-06 22:18:42 +0200

    Plan v1

--- src/app/components/plan/plan.component.ts
+++ src/app/components/plan/plan.component.ts
@@ -3,6 +3,7 @@ import { replanAPIService } from '../../services/replanAPI.service';
 import { ActivatedRoute } from '@angular/router';
 
 declare var $: any;
+declare const google: any;
 
 @Component({
   selector: 'app-plan',
@@ -14,6 +15,8 @@ export class PlanComponent implements OnInit {
   idRelease: number;
   plan: any;
 
+  chartRows: any[];
+
   constructor(private _replanAPIService: replanAPIService,
               private activatedRoute: ActivatedRoute) {
 
@@ -23,15 +26,47 @@ export class PlanComponent implements OnInit {
 
                   this._replanAPIService.getReleasePlan(this.idProject, this.idRelease)
                     .subscribe( data => {
+                      debugger; 
                       this.plan = data;
+                      this.chartLogic(this.plan);
                     });
               });
 
   }
 
   ngOnInit() {
-    debugger;
     $('li.nav-item').removeClass('active');
+  }
+
+  chartLogic(data) {
+    this.chartRows = [];
+    data.jobs.forEach(job => {
+      const starts = new Date(job.starts);
+      const ends = new Date(job.ends);
+      const row = [
+        job.resource.name,
+        job.feature.name,
+        new Date(starts.getFullYear(), starts.getMonth(), starts.getDate()),
+        new Date(ends.getFullYear(), ends.getMonth(), ends.getDate())
+      ];
+      this.chartRows.push(row);
+    });
+    this.drawChart(this.chartRows);
+  }
+
+  drawChart(rows) {
+    debugger;
+    if (rows.length > 0) {
+      const container = document.getElementById('timeline');
+      const chart = new google.visualization.Timeline(container);
+      const dataTable = new google.visualization.DataTable();
+      dataTable.addColumn({ type: 'string', id: 'Resource' });
+      dataTable.addColumn({ type: 'string', id: 'Feature' });
+      dataTable.addColumn({ type: 'date', id: 'Start' });
+      dataTable.addColumn({ type: 'date', id: 'End' });
+      dataTable.addRows(rows);
+      chart.draw(dataTable);
+    }
   }
 
 }
